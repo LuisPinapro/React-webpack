2a06a44523f16b7c17653db515fdb026818d7ef7
LuisPinapro <luis30002011@hotmail.com>
2022-07-11 21:49:50 -0500

    Se termino el curso react

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import App from './components/App';
+import { createRoot } from 'react-dom/client';
+import './styles/global.scss'
+const container = document.getElementById('app');
+const root = createRoot(container);
+root.render(<App/>);
+
+
